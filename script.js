cde0a995770dddab8b74274538d5a7cd3fee5c2f
LucasS <lm.spizzirri@gmail.com>
2022-10-06 17:37:54 -0300

    Ex 6 e 7

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-window.onload = function () {
-    const button = document.getElementById('button-random-color');
-    const board = document.getElementById('pixel-board');
-    let pcselected = document.querySelectorAll('.color');
-    let colors = ['#000000'];
-    button.addEventListener('click', function () {
-        let cor = '#';
-        let letters = '0123456789ABCDEF';
-        for (index = 1; index < 4; index += 1) {
-            cor = '#';
-            for (let i = 0; i < 7; i += 1) {
-                cor += letters[Math.floor(Math.random() * 16)];
-                pcselected[index].style.backgroundColor = cor;
-            }
-        }
-    })
-}
+const button = document.getElementById('button-random-color');
+const pcselected = document.querySelectorAll('.color');
+let cor = '#';
+const letters = '0123456789ABCDEF';
+const cores = ['#000000'];
+button.addEventListener('click', () => {
+  for (let index = 0; index < 4; index += 1) {
+    cor = '#';
+    if (index === 0) {
+      pcselected[index].style.backgroundColor = '#000000';
+    }
+    else {
+      for (let i = 1; i < 7; i += 1) {
+        cor += letters[Math.floor(Math.random() * 16)];
+        pcselected[index].style.backgroundColor = cor;
+        cores[index] = cor;
+      }
+    }
+    window.localStorage.setItem('colorPalette', JSON.stringify(cores));
+  }
+});
 
+function colorStorage() {
+  const recoveredColors = JSON.parse(window.localStorage.getItem('colorPalette'));
+  for (let i = 0; i < pcselected.length; i += 1) {
+    pcselected[i].style.backgroundColor = recoveredColors[i];
+  }
+}
+function fillBoard() {
+  const board = document.getElementById('pixel-board');
+  for (let index = 0; index < 25; index += 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    board.appendChild(pixel);
+  }
+}
 
 // Preenchendo o quadro de pixels
-const line = document.getElementsByClassName('line');
-fillBoard(line);
-
-function createpixel(classIdentify) {
-    const pixel = document.createElement('div');
-    pixel.className = classIdentify;
-    pixel.style.backgroundColor = 'white';
-    pixel.style.border = '10px solid red';
-    return pixel;
-}
-function fillLine(divLine) {
-    for (let i = 1; i < 6; i += 1) {
-        const pixel = createpixel('pixel');
-        divLine.appendChild(pixel);
-    }
-}
-function fillBoard(lines) {
-    for (let i = 0; i < lines.length; i += 1) {
-        fillLine(lines[i]);
-    }
+// Executando funções
+document.onload = function (){
+colorStorage();
 }
 
-console.log(line);
+fillBoard();
